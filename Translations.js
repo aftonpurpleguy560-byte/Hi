b3e5de65d242faff68f7d41a69808698daed4a4f
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-02-16 21:56:44 +0300

    Update Translations.js

--- Translations.js
+++ Translations.js
@@ -1,22 +1,22 @@
 /**
  * Purpleguy © 2026 - tablet power
- * Dil Dosyası
+ * Dil Dosyası - Türkçe ve İngilizce metinleri içerir.
  */
 const translations = {
     tr: {
         title: "Merhaba, Ben <span class='highlight'>Efe</span>",
         subtitle: "GAMER & DEVELOPER",
         bio: "YouTube kanalımda popüler oyunların videolarını, rehberlerini ve en komik anlarını paylaşıyorum. Aynı zamanda yazılım dünyasında kendimi geliştiriyorum.",
-        btnYt: "Kanalıma Git",
-        btnInsta: "Instagram Hesabıma Git", // Türkçe metin
+        btnYt: "Youtube Kanalıma Git",
+        btnInsta: "Instagram Hesabıma Git",
         btnLang: "İngilizce"
     },
     en: {
         title: "Hi, I'm <span class='highlight'>Efe</span>",
         subtitle: "GAMER & DEVELOPER",
         bio: "On my YouTube channel, I share gameplay videos, guides, and the funniest moments of popular games. I'm also developing myself in the world of software.",
-        btnYt: "Visit My Channel",
-        btnInsta: "Go to My Instagram", // İngilizce metin
+        btnYt: "Visit My Youtube",
+        btnInsta: "Go to My Instagram",
         btnLang: "Turkish"
     }
 };
